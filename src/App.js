8b424d0f75baa8e23a701bdea96f7785cb4d2343
jaka-sketch <74190783+jaka-sketch@users.noreply.github.com>
2020-11-18 21:19:57 +0700

    emotion theming

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./styles.css";
+import { Themeprovider } from "@emotion/react";
 
 import TodoList from "./pages/TodoList";
 
+const theme = {
+  color: {
+    primary: {
+      black: "#484848",
+      red: "#e06262"
+    }
+  }
+};
 export default function App() {
-  return <TodoList />;
+  return (
+    <Themeprovider theme={theme}>
+      <TodoList />
+    </Themeprovider>
+  );
 }
