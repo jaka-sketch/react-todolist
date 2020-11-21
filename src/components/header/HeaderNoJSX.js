a6e9dc153aec97e8822487ebbddc5decea98c8f0
jaka-sketch <74190783+jaka-sketch@users.noreply.github.com>
2020-11-21 20:15:57 +0700

    No JSX header

---
+++ src/components/header/HeaderNoJSX.js
@@ -0,0 +1,74 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import PropTypes from "prop-types";
+import { useTheme } from "@emotion/react";
+
+import Button from "../button/ButtonNoJSX";
+import Container from "../../layout/Container";
+import Item from "../../layout/Item";
+
+import * as styles from "./header.styles";
+
+const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  const theme = useTheme();
+
+  return jsx(
+    "section",
+    {
+      className: "header-component"
+    },
+    jsx(
+      Container,
+      {
+        alignItems: "flex-start"
+      },
+      jsx(
+        //Item pertama
+        Item,
+        {
+          flex: 1
+        },
+        jsx(Button, {
+          text: showAdd ? "Finish" : "Add",
+          onClick: showAddToggle
+        })
+      ),
+      jsx(
+        //Item Kedua
+        Item,
+        {
+          flex: 2
+        },
+        jsx(
+          "h1",
+          {
+            css: styles.headerTitle(theme)
+          },
+          "Todo Lists"
+        )
+      ),
+      jsx(
+        //Item Ketiga
+        Item,
+        {
+          flex: 1,
+          align: "right"
+        },
+        jsx(Button, {
+          text: "Clear",
+          onClick: clearTodos,
+          color: "red",
+          align: "right"
+        })
+      )
+    )
+  );
+};
+
+Header.propTypes = {
+  showAddToggle: PropTypes.func.isRequired,
+  showAdd: PropTypes.func.isRequired,
+  clearTodos: PropTypes.func.isRequired
+};
+
+export default Header;
